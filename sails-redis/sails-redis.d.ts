557460f4ed75a9ffbb58fcad2818b5d1cde7e432
Samuel Marks <samuelmarks@gmail.com>
2016-05-05 15:27:02 +1000

    Remove abstraction

--- sails-redis/sails-redis.d.ts
+++ sails-redis/sails-redis.d.ts
@@ -1,20 +1,12 @@
-declare var sailsRedis: sailsRedis.sailsRedis;
-
-declare namespace sailsRedis {
-    export interface sailsRedis {
-        new (): sailsRedis;
-        registerConnection: (connection: any, collections: any, cb: any) => any;
-        teardown: (conn: any, cb: any) => any;
-        query: (connectionName: any, table: any, query: any, data: any, cb: any) => any;
-        describe: (connectionName: any, table: any, cb: any) => any;
-        define: (connectionName: any, table: any, definition: any, cb: any) => any;
-        drop: (connectionName: any, table: any, relations: any, cb: any) => any;
-        addAttribute: (connectionName: any, table: any, attrName: any, attrDef: any, cb: any) => any;
-        removeAttribute: (connectionName: any, table: any, attrName: any, cb: any) => any;
-        /* etc. */
-    }
-}
-
-declare module "sails-redis" {
-    export = sailsRedis;
+export interface sailsRedis {
+    new (): sailsRedis;
+    registerConnection: (connection: any, collections: any, cb: any) => any;
+    teardown: (conn: any, cb: any) => any;
+    query: (connectionName: any, table: any, query: any, data: any, cb: any) => any;
+    describe: (connectionName: any, table: any, cb: any) => any;
+    define: (connectionName: any, table: any, definition: any, cb: any) => any;
+    drop: (connectionName: any, table: any, relations: any, cb: any) => any;
+    addAttribute: (connectionName: any, table: any, attrName: any, attrDef: any, cb: any) => any;
+    removeAttribute: (connectionName: any, table: any, attrName: any, cb: any) => any;
+    /* etc. */
 }
