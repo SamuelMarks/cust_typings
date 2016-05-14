21e4ce1b4c39b63051820cb9a078707decfdb919
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 15:14:43 +1000

    Overload namespace

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -1,6 +1,7 @@
+import {LanguageChains, NumericComparison, TypeComparison} from '~chai/lib/Assertion';
 declare function chaiJsonSchema(chai: any, utils: any): void;
 
-declare namespace chaiJsonSchema {
+declare namespace Chai {
     export function formatResult(error: string, data: string, schema: string, indent: string): string;
 
     export function extractSchemaLabel(schema: string, max: number): string;
@@ -11,6 +12,28 @@ declare namespace chaiJsonSchema {
         jsonSchema(j: JSON): Assertion;
         jsonSchema(o: {}): Assertion;
     }
+
+    // For BDD API
+    interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
+        eventually: Assertion;
+        become(expected: any): Assertion;
+        fulfilled: Assertion;
+        rejected: Assertion;
+        rejectedWith(expected: any, message?: string | RegExp): Assertion;
+        notify(fn: Function): Assertion;
+        jsonSchema(j: JSON): Assertion;
+        jsonSchema(o: {}): Assertion;
+    }
+
+    // For Assert API
+    interface Assert {
+        jsonSchema: Assert;
+        jsonSchema(j: JSON): Assert;
+        jsonSchema(o: {}): Assert;
+    }
+}
+
+namespace chaiJsonSchema {
 }
 
 export = chaiJsonSchema;
