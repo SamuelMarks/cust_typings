a3705944162b97351b5a7a57f1a5b99d9b653f59
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 18:09:45 +1000

    Trying a different abstraction approach

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -1,39 +1,23 @@
-import {LanguageChains, NumericComparison, TypeComparison} from '~chai/lib/Assertion';
-declare function chaiJsonSchema(chai: any, utils: any): void;
+declare module 'chai-json-schema' {
+    function chaiJsonSchema(chai: any, utils: any): void;
+    namespace chaiJsonSchema {
+        export function formatResult(error: string, data: string, schema: string, indent: string): string;
+
+        export function extractSchemaLabel(schema: string, max: number): string;
+
+        export function valueStrim(value: string, cutoff?: number): string;
+    }
+    export = chaiJsonSchema;
+}
 
 declare namespace Chai {
-    export function formatResult(error: string, data: string, schema: string, indent: string): string;
-
-    export function extractSchemaLabel(schema: string, max: number): string;
-
-    export function valueStrim(value: string, cutoff?: number): string;
-
-    export interface Assertion {
-        jsonSchema(j: JSON): Assertion;
-        jsonSchema(o: {}): Assertion;
-    }
-
     // For BDD API
-    interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
-        eventually: Assertion;
-        become(expected: any): Assertion;
-        fulfilled: Assertion;
-        rejected: Assertion;
-        rejectedWith(expected: any, message?: string | RegExp): Assertion;
-        notify(fn: Function): Assertion;
-        jsonSchema(j: JSON): Assertion;
-        jsonSchema(o: {}): Assertion;
+    interface Assertion {
+        jsonSchema(j: JSON|{}): Assertion;
     }
 
     // For Assert API
     interface Assert {
-        jsonSchema: Assert;
-        jsonSchema(j: JSON): Assert;
-        jsonSchema(o: {}): Assert;
+        jsonSchema(j: JSON|{}): Assert;
     }
 }
-
-namespace chaiJsonSchema {
-}
-
-export = chaiJsonSchema;
