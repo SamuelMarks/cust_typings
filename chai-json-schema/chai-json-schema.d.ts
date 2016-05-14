04ffcf27b0e50681edab1e6ffdc2583e8feea71a
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 12:14:22 +1000

    Rewriting chai-json-schema scaffold

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -1,3 +1,11 @@
-export function formatResult(error: string, data: string, schema: string, indent: string): string;
-export function extractSchemaLabel(schema: string, max: number): string;
-export function valueStrim(value: string, cutoff?: number): string;
+declare function chaiJsonSchema(chai: any, utils: any): void;
+
+declare namespace chaiJsonSchema {
+    export function formatResult(error: string, data: string, schema: string, indent: string): string;
+
+    export function extractSchemaLabel(schema: string, max: number): string;
+
+    export function valueStrim(value: string, cutoff?: number): string;
+}
+
+export = chaiJsonSchema;
