c40b498b3994b107b056a9db4921b54857fc5e85
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 18:18:54 +1000

    Comment is read?

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -1,15 +1,5 @@
 declare function chaiJsonSchema(chai: any, utils: any): void;
 
-/*
-declare module chaiJsonSchema {
-    export function formatResult(error: string, data: string, schema: string, indent: string): string;
-
-    export function extractSchemaLabel(schema: string, max: number): string;
-
-    export function valueStrim(value: string, cutoff?: number): string;
-}
-*/
-
 declare namespace Chai {
     // For BDD API
     interface Assertion {
