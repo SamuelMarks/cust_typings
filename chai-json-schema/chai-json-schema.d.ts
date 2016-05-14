2bde22dca9dcf24ad855e8e2c568316cd5330c10
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 18:14:46 +1000

    Trying a different abstraction approach

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -1,13 +1,11 @@
 declare module chaiJsonSchema {
     function chaiJsonSchema(chai: any, utils: any): void;
 
-    namespace chaiJsonSchema {
-        export function formatResult(error: string, data: string, schema: string, indent: string): string;
+    export function formatResult(error: string, data: string, schema: string, indent: string): string;
 
-        export function extractSchemaLabel(schema: string, max: number): string;
+    export function extractSchemaLabel(schema: string, max: number): string;
 
-        export function valueStrim(value: string, cutoff?: number): string;
-    }
+    export function valueStrim(value: string, cutoff?: number): string;
 }
 
 declare namespace Chai {
