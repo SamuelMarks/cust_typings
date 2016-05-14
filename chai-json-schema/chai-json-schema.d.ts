af0d4a3e50a2294877c13dff68d5422beb3d5d16
Samuel Marks <samuelmarks@gmail.com>
2016-05-14 12:05:50 +1000

    Added chai-json-schema from https://github.com/SamuelMarks/DefinitelyTyped/blob/8ee6326bd5b4d082657dce4c9c0060cb895c7e65/chai-json-schema/chai-json-schema.d.ts

---
+++ chai-json-schema/chai-json-schema.d.ts
@@ -0,0 +1,3 @@
+export function formatResult(error: string, data: string, schema: string, indent: string): string;
+export function extractSchemaLabel(schema: string, max: number): string;
+export function valueStrim(value: string, cutoff?: number): string;
