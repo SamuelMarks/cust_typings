9605cb28a4dd63b0d1c6d9c7b425347416fe4817
Samuel Marks <samuelmarks@gmail.com>
2016-05-16 14:23:03 +1000

    Trying a different abstraction approach

--- chai-json-schema/chai-json-schema.d.ts
+++ chai-json-schema/chai-json-schema.d.ts
@@ -5,17 +5,17 @@ declare module chaiJsonSchema {
     export interface chaiJsonSchema {
         (chai: any, utils: any): void
     }
+}
 
-    namespace Chai {
-        // For BDD API
-        interface Assertion {
-            jsonSchema(j: JSON|{}): Assertion;
-        }
+namespace Chai {
+    // For BDD API
+    interface Assertion {
+        jsonSchema(j: JSON|{}): Assertion;
+    }
 
-        // For Assert API
-        interface Assert {
-            jsonSchema(j: JSON|{}): Assert;
-        }
+    // For Assert API
+    interface Assert {
+        jsonSchema(j: JSON|{}): Assert;
     }
 }
 
